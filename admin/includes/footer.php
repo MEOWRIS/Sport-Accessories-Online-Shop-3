a995901eb18d91e4def9b77fc6d370a57f61cb3f
MEOWRIS <117509123+MEOWRIS@users.noreply.github.com>
2022-11-11 03:16:14 +0800

    Github Upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Last Modified 4/11/2022</b>
+    </div>
+    <strong>Copyright &copy; 2022 Brought To You By<a href="#"> Harris Irfan</a></strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Last Modified 4/11/2022</b>
+      </div>
+      <strong>Copyright &copy; 2022 Brought to You By <a href="#">Harris Irfan</a></strong>
+    </div>
+</footer>
